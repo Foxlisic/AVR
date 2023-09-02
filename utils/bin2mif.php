f2678ec70d4648b3f95c229ba572c64bc4285530
FoxLib <hplus.foxtail@gmail.com>
2023-09-02 07:17:39 +0300

    DA

--- utils/bin2mif.php
+++ utils/bin2mif.php
@@ -1,20 +1,47 @@
 <?php
 
-/**
- * Преобразует бинарные данные в MEMFLASH.MIF
+/*
+ * Конвертирование из Bin -> MIF файл
+ * Аргумент 1: Размер памяти (256k = 262144)
+ * Аргумент 2: bin-файл
+ * Аргумент 3: Куда выгрузить
  */
 
-$file = isset($argv[1]) ? $argv[1] : stdin;
-$file = file_get_contents($file);
-$size = (int)(strlen($file) >> 1);
+$size = (int) $argv[1];
+$data = file_get_contents($argv[2]);
+$len  = strlen($data);
+if ($size < 1024) $size *= 1024;
 
-$out = "WIDTH=16;\nDEPTH=65536;\nADDRESS_RADIX=HEX;\nDATA_RADIX=HEX;\nCONTENT BEGIN\n";
-for ($i = 0; $i < $size; $i++) {
+if (empty($size)) { echo "size required\n"; exit(1); }
 
-    $val = ord($file[2*$i]) + 256*ord($file[2*$i + 1]);
-    $out .= sprintf("  %04X: %04X;\n", $i, $val);
+$out = [
+    "WIDTH=8;",
+    "DEPTH=$size;",
+    "ADDRESS_RADIX=HEX;",
+    "DATA_RADIX=HEX;",
+    "CONTENT BEGIN",
+];
+
+$a = 0;
+
+// RLE-кодирование
+while ($a < $len) {
+
+    // Поиск однотонных блоков
+    for ($b = $a + 1; $b < $len && $data[$a] == $data[$b]; $b++);
+
+    // Если найденный блок длиной до 0 до 2 одинаковых символов
+    if ($b - $a < 3) {
+        for ($i = $a; $i < $b; $i++) $out[] = sprintf("  %X: %02X;", $a++, ord($data[$i]));
+    } else {
+        $out[] = sprintf("  [%X..%X]: %02X;", $a, $b - 1, ord($data[$a]));
+        $a = $b;
+    }
 }
-$out .= sprintf("  [%04X..FFFF]: 0000;\n", $size);
-$out .= "END;\n";
 
-if (isset($argv[2])) file_put_contents($argv[2], $out); else echo $out;
+if ($len < $size) $out[] = sprintf("  [%X..%X]: 00;", $len, $size-1);
+$out[] = "END;";
+$pb = join("\n", $out);
+
+// Сохранить информацию
+if (isset($argv[3])) file_put_contents($argv[3], $pb); else echo $pb;
